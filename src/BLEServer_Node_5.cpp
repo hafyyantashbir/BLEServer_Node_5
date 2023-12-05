5baaecaedeecb9042db50f9537498ae8f50c1e6a
hafyyantashbir <hafyyantashbir@gmail.com>
2023-12-05 16:22:54 +0700

    update

--- src/BLEServer_Node_5.cpp
+++ src/BLEServer_Node_5.cpp
@@ -14,6 +14,17 @@
 #include <RF24Mesh.h>
 #include "printf.h"
 
+//konfigurasi us-100
+//sumbu X
+#define TRIGGER1 33
+#define ECHO1    25
+//sumbu Y
+#define TRIGGER2 32
+#define ECHO2 35
+//sumbu Z
+#define TRIGGER3 27
+#define ECHO3    26
+
 //konfigurasi stack size
 SET_LOOP_TASK_STACK_SIZE(64*1024); // 64KB
 
@@ -61,6 +72,13 @@ unsigned long currentMillis = 0;
 void setup() {
   Serial.begin(115200);
 
+  pinMode(TRIGGER1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(TRIGGER2, OUTPUT);
+  pinMode(ECHO2, INPUT);
+  pinMode(TRIGGER3, OUTPUT);
+  pinMode(ECHO3, INPUT);
+
   //Fungsi untuk 2 loop
   //  xTaskCreatePinnedToCore(
   //    loop2,
@@ -105,29 +123,64 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
-
   // print sisa memori stack pada void loop
   Serial.printf("\nLoop() - Free Stack Space: %d", uxTaskGetStackHighWaterMark(NULL));
   
   mesh.update();
   DateTime now = rtc.now();
-  StaticJsonDocument<96> doc;
+  StaticJsonDocument<128> doc;
 
   // Mengirim data ke master
   if (millis() - currentMillis >= 1000) {
     currentMillis = millis();
-    doc["NodeID"] = node_asal;
-    doc["usX"] = usX;
-    doc["usY"] = usY;
-    doc["usZ"] = usZ;
-    doc["Unixtime"] = now.unixtime();
+    
+    //sumbu X
+    long duration1;
+    long distance1;
+    digitalWrite(TRIGGER1, LOW);
+    delayMicroseconds(2); 
+    digitalWrite(TRIGGER1, HIGH);
+    delayMicroseconds(10); 
+    digitalWrite(TRIGGER1, LOW);
+    duration1 = pulseIn(ECHO1, HIGH);
+    distance1 = duration1 * 0.34 / 2;
+    usX = distance1;
+
+    //sumbu Y
+    long duration2;
+    long distance2;
+    digitalWrite(TRIGGER2, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGGER2, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER2, LOW);
+    duration2 = pulseIn(ECHO2, HIGH);
+    distance2 = duration2 * 0.34 / 2;
+    usY = distance2;
+
+    //sumbu Z
+    long duration3;
+    long distance3;
+    digitalWrite(TRIGGER3, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGGER3, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER3, LOW);
+    duration3 = pulseIn(ECHO3, HIGH);
+    distance3 = duration3 * 0.34 / 2;
+    usZ = distance3;
+
+    doc["NodeID"] = String(node_asal);
+    doc["usX"] = String(usX);
+    doc["usY"] = String(usY);
+    doc["usZ"] = String(usZ);
+    doc["Unixtime"] = String(now.unixtime());
     datakirim = "";
     serializeJson(doc, datakirim);
     char kirim_loop[datakirim.length() + 1];
     datakirim.toCharArray(kirim_loop, sizeof(kirim_loop));
 
-    if (!mesh.write(&kirim_loop, 'M', sizeof(kirim_loop))) {
+    if (!mesh.write(&kirim_loop, '5', sizeof(kirim_loop))) {
       if (!mesh.checkConnection()) {
         Serial.println("Memperbaharui Alamat");
         if (mesh.renewAddress() == MESH_DEFAULT_ADDRESS) {
